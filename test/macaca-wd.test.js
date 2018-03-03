bbbeab76b618784ac440487fe0167b097ddec7f4
xudafeng <xudafeng@126.com>
2018-03-04 01:16:49 +0800

    add reporter

--- test/macaca-wd.test.js
+++ test/macaca-wd.test.js
@@ -1,9 +1,11 @@
 'use strict';
 
-var wd = require('../lib/macaca-wd.js');
+const assert = require('assert');
+
+const wd = require('../lib/macaca-wd.js');
 
 describe('test', function() {
   it('should be ok', function() {
-    wd.should.be.ok;
+    assert.ok(wd);
   });
 });
