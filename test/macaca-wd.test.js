35c4534dcbe4fa804894a4e3936aa9cd3a4989b6
Zhu Liang <zhuliangg11@gmail.com>
2019-03-16 23:24:54 +0800

    feat(test): fix test process exit, add test for get (#63)
    * feat(test): fix test process exit, add test for get

--- test/macaca-wd.test.js
+++ test/macaca-wd.test.js
@@ -8,14 +8,14 @@ const wd = require('../lib/macaca-wd');
 
 describe('test/macaca-wd.test.js', function() {
   let driver, server;
-  beforeEach(() => {
+  before(() => {
     server = new Server();
     server.start();
     const remoteConfig = {
       host: 'localhost',
       port: 3456
     };
-    
+
     driver = wd.promiseChainRemote(remoteConfig);
     driver.configureHttp({
       timeout: 20 * 1000,
@@ -24,16 +24,21 @@ describe('test/macaca-wd.test.js', function() {
     });
   });
 
-  afterEach(() => {
+  after(() => {
     server.stop();
-    process.exit(0);
   });
 
   it('init should be ok', async () => {
     await driver.init({
       platformName: 'desktop',
-      browserName: 'chrome',
+      browserName: 'chrome'
     });
     assert.equal(server.ctx.url, '/wd/hub/session');
+  });
+
+  it('get is ok', async () => {
+    await driver.get('baidu.com');
+    assert.equal(server.ctx.url, '/wd/hub/session/sessionId/url');
+    assert.equal(server.ctx.request.body.url, 'baidu.com');
   });
 });
