6ef90350fabfcc9292630d6b4cf0d89aa507df49
达峰的夏天 <xudafeng@126.com>
2019-03-17 15:01:57 +0800

    test: testcase supplement (#67)

---
+++ test/screenshot.test.js
@@ -0,0 +1,65 @@
+'use strict';
+
+const assert = require('assert');
+
+const { Server } = require('./helper');
+
+const wd = require('../lib/macaca-wd');
+
+describe('test/screenshot.test.js', function() {
+  let driver, server;
+  before(() => {
+    server = new Server();
+    server.start();
+    const remoteConfig = {
+      host: 'localhost',
+      port: 3456
+    };
+
+    driver = wd.promiseChainRemote(remoteConfig);
+    driver.configureHttp({
+      timeout: 20 * 1000,
+      retries: 5,
+      retryDelay: 5
+    });
+  });
+
+  after(() => {
+    server.stop();
+  });
+
+  /**
+   * https://macacajs.github.io/macaca-wd/#saveScreenshot
+   */
+  describe('saveScreenshot', async () => {
+    it('should work', async () => {
+      await driver.saveScreenshot();
+      assert.equal(server.ctx.method, 'GET');
+      assert.equal(server.ctx.url, '/wd/hub/session/screenshot');
+      assert.deepEqual(server.ctx.request.body, {});
+      assert.deepEqual(server.ctx.response.body, {
+        sessionId: 'sessionId',
+        status: 0,
+        value: '',
+      });
+    });
+  });
+
+  /**
+   * https://macacajs.github.io/macaca-wd/#takeScreenshot
+   */
+  describe('takeScreenshot', async () => {
+    it('should work', async () => {
+      await driver.takeScreenshot();
+      assert.equal(server.ctx.method, 'GET');
+      assert.equal(server.ctx.url, '/wd/hub/session/screenshot');
+      assert.deepEqual(server.ctx.request.body, {});
+      assert.deepEqual(server.ctx.response.body, {
+        sessionId: 'sessionId',
+        status: 0,
+        value: '',
+      });
+    });
+  });
+});
+
---
+++ test/url.test.js
@@ -0,0 +1,48 @@
+'use strict';
+
+const assert = require('assert');
+
+const { Server } = require('./helper');
+
+const wd = require('../lib/macaca-wd');
+
+describe('test/url.test.js', function() {
+  let driver, server;
+  before(() => {
+    server = new Server();
+    server.start();
+    const remoteConfig = {
+      host: 'localhost',
+      port: 3456
+    };
+
+    driver = wd.promiseChainRemote(remoteConfig);
+    driver.configureHttp({
+      timeout: 20 * 1000,
+      retries: 5,
+      retryDelay: 5
+    });
+  });
+
+  after(() => {
+    server.stop();
+  });
+
+  /**
+   * https://macacajs.github.io/macaca-wd/#url
+   */
+  describe('url', async () => {
+    it('should work', async () => {
+      await driver.url();
+      assert.equal(server.ctx.method, 'GET');
+      assert.equal(server.ctx.url, '/wd/hub/session/url');
+      assert.deepEqual(server.ctx.request.body, {});
+      assert.deepEqual(server.ctx.response.body, {
+        sessionId: 'sessionId',
+        status: 0,
+        value: '',
+      });
+    });
+  });
+});
+
