83fba0fb30d53f9747dfe7137b0fbd0d86da3cb4
Zhu Liang <zhuliangg11@gmail.com>
2019-03-17 13:56:48 +0800

    test(cookie): add cookie tests

--- test/cookie.test.js
+++ test/cookie.test.js
@@ -40,34 +40,65 @@ describe('test/cookie.test.js', function() {
    */
   describe('allCookies', async () => {
     it('should work', async () => {
-
+      await driver.allCookies();
+      assert.equal(server.ctx.url, '/wd/hub/session/sessionId/cookie');
+      assert.equal(server.ctx.method, 'GET');
     });
   });
 
-    /**
+  /**
    * https://macacajs.github.io/macaca-wd/#deleteAllCookies
    */
   describe('deleteAllCookies', async () => {
     it('should work', async () => {
-
+      await driver.deleteAllCookies();
+      assert.equal(server.ctx.url, '/wd/hub/session/sessionId/cookie');
+      assert.equal(server.ctx.method, 'DELETE');
+      assert.deepEqual(server.ctx.request.body, {});
+      assert.deepEqual(server.ctx.response.body, {
+        sessionId: 'sessionId',
+        status: 0,
+        value: ''
+      });
     });
   });
 
-    /**
+  /**
    * https://macacajs.github.io/macaca-wd/#deleteCookie
    */
   describe('deleteCookie', async () => {
     it('should work', async () => {
-
+      await driver.deleteCookie('test_cookie');
+      assert.equal(
+        server.ctx.url,
+        '/wd/hub/session/sessionId/cookie/test_cookie'
+      );
+      assert.equal(server.ctx.method, 'DELETE');
+      assert.deepEqual(server.ctx.request.body, {});
+      assert.deepEqual(server.ctx.response.body, {
+        sessionId: 'sessionId',
+        status: 0,
+        value: ''
+      });
     });
   });
 
-    /**
+  /**
    * https://macacajs.github.io/macaca-wd/#setCookie
    */
   describe('setCookie', async () => {
     it('should work', async () => {
-
+      await driver.setCookie('test_cookie');
+      assert.equal(server.ctx.url, '/wd/hub/session/sessionId/cookie');
+      assert.equal(server.ctx.method, 'POST');
+      assert.deepEqual(server.ctx.request.body, {
+        cookie: 'test_cookie'
+      });
+      assert.deepEqual(server.ctx.response.body, {
+        sessionId: 'sessionId',
+        status: 0,
+        value: ''
+      });
     });
   });
 });
