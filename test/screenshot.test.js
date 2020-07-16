bb64a0b8aec2d775ff5056ab070cb25b91073d75
zivyangll <zivyangll@gmail.com>
2020-07-16 16:20:11 +0800

    fix: test

--- test/screenshot.test.js
+++ test/screenshot.test.js
@@ -34,7 +34,7 @@ describe('test/screenshot.test.js', function() {
   describe('saveScreenshot', async () => {
     it('should work', async () => {
       await driver.saveScreenshot();
-      assert.equal(server.ctx.method, 'GET');
+      assert.equal(server.ctx.method, 'POST');
       assert.equal(server.ctx.url, '/wd/hub/session/screenshot');
       assert.deepEqual(server.ctx.request.body, {});
       assert.deepEqual(server.ctx.response.body, {
@@ -50,8 +50,10 @@ describe('test/screenshot.test.js', function() {
    */
   describe('takeScreenshot', async () => {
     it('should work', async () => {
-      await driver.takeScreenshot();
-      assert.equal(server.ctx.method, 'GET');
+      await driver.takeScreenshot('', {
+        fullPage: true,
+      });
+      assert.equal(server.ctx.method, 'POST');
       assert.equal(server.ctx.url, '/wd/hub/session/screenshot');
       assert.deepEqual(server.ctx.request.body, {});
       assert.deepEqual(server.ctx.response.body, {
